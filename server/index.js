f3b4f8d84764a13d4839e59cd4a54bf68663b354
Matheus Galdino <matheus.galdino@hotmail.com>
2018-01-22 15:35:02 -0200

    Updating server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const app = require('./src/app');
 
-const port = process.env.port || 3001;
+const port = process.env.PORT || 3001;
 
-app.listen(port, () => {
+app.listen(port, (err) => {
+  if (err) console.log(`Error: ${err}`);
   console.log(`Running at port ${port}`);
 });
